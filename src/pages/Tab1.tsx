aee6810ec5e1f99d031ba53acae6eb7e96bfb6dd
hanny <hannajanemanlangit33@gmail.com>
2024-03-08 09:04:25 +0800

    change all

---
+++ src/pages/Tab1.tsx
@@ -0,0 +1,35 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab1.css';
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>profile</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">profile</IonTitle>
+          </IonToolbar>
+
+          
+        </IonHeader>
+        <IonCard>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+      <IonCardHeader>
+        <IonCardTitle>hanny</IonCardTitle>
+        <IonCardSubtitle>student</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent> Nothing more, nothing less.</IonCardContent>
+    </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
