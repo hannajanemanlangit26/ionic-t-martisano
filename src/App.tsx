e5da0c6b4b57f6e835fd5ccb905ce1c49c792c7e
hanny <hannajanemanlangit33@gmail.com>
2024-03-19 08:31:33 +0800

    added a new icon

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { calculator, ellipse, homeOutline, personAddOutline, personOutline, square, triangle } from 'ionicons/icons';
+import { calculator, ellipse, homeOutline, personAddOutline, personOutline, speedometerOutline, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -67,7 +67,7 @@ const App: React.FC = () => (
             <IonLabel>profile</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
+            <IonIcon aria-hidden="true" icon={speedometerOutline} />
             <IonLabel>click counter</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
