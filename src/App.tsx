7361509bb32ecdbf4300b87caa122ffea356a871
hanny <hannajanemanlangit33@gmail.com>
2024-03-19 08:39:25 +0800

    added a resource folder

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,11 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { calculator, ellipse, homeOutline, personAddOutline, personOutline, speedometerOutline, square, triangle } from 'ionicons/icons';
+import Home from './pages/Home';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+
 
 
 /* Core CSS required for Ionic components to work properly */
